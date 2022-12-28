1b34f5593ce0892f739a366753c8074af8bec259
jaehoon-Han <eroel1060@daum.net>
2022-12-28 19:59:36 +0900

    unit test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -10,9 +11,115 @@ describe('UsersService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+     service.create({
+        name: '재훈이',
+        gender: '남자',
+        age: 29,
+        password: 'wkdk',
+        medicalHistory: ['두통', '치통']
+      });
   });
+
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll", () => {
+    it("should return an array", () => {
+
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe("getOne", () => {
+    it("should return a user", () => {
+      service.create({
+        name: '재훈이',
+        gender: '남자',
+        age: 29,
+        password: 'wkdk',
+        medicalHistory: ['두통', '치통']
+      });
+      const user = service.getOne(1);
+      expect(user).toBeDefined();
+    });
+
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('User with ID 999 not found.');
+      }
+    });
+  });
+
+
+  describe("deleteOne", () => {
+
+    it("delete a user", () => {
+      service.create({
+        name: '재훈이',
+        gender: '남자',
+        age: 29,
+        password: 'wkdk',
+        medicalHistory: ['두통', '치통']
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1)
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it("should return a 404", () => {
+      try {
+        service.deleteOne(999)
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe("create", () => {
+    it("should create a user", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        name: '재훈이',
+        gender: '남자',
+        age: 29,
+        password: 'wkdk',
+        medicalHistory: ['두통', '치통']
+      });
+      const afterCreate = service.getAll().length;
+      console.log(beforeCreate, afterCreate);
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe("update", () => {
+
+    it("shuold update a user", () => {
+      service.create({
+        name: '재훈이',
+        gender: '남자',
+        age: 29,
+        password: 'wkdk',
+        medicalHistory: ['두통', '치통']
+      });
+      service.update(1, {name: '한재훈'});
+      const user = service.getOne(1);
+      expect(user.name).toEqual('한재훈');
+    });
+    it('shoul throw a NotFoundException', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
+
+
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+     }),
+    );
     await app.init();
   });
 
@@ -19,6 +25,72 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Maeum API');
   });
+
+  describe("/users", () => {
+    it("GET", () => {
+      return request(app.getHttpServer())
+        .get("/users")
+        .expect(200)
+        .expect([]);
+    });
+    it("POST 201", () => {
+      return request(app.getHttpServer())
+        .post("/users")
+        .send({
+          name: "Test",
+          age: 11,
+          gender: "female",
+          password: "test",
+          medicalHistory: ["test", "test"]
+        })
+        .expect(201);
+    });
+  
+  it("POST 400", () => {
+    return request(app.getHttpServer())
+      .post("/users")
+      .send({
+        name: "Test",
+        age: 11,
+        gender: "female",
+        password: "test",
+        medicalHistory: ["test", "test"],
+        other: "thing"
+      })
+      .expect(400);
+  });
+  it("DELETE", () => {
+    return request(app.getHttpServer())
+      .delete('/users')
+      .expect(404);
+  });
+});
+
+
+describe('/users/:id', () => {
+  it('GET 200', () => {
+    return request(app.getHttpServer())
+      .get('/users/1')
+      .expect(200);
+  });
+  it('GET 404', () => {
+    return request(app.getHttpServer())
+      .get('/users/111')
+      .expect(404);
+  });
+  it('PATCH 200', () => {
+    return request(app.getHttpServer())
+      .patch('/users/1')
+      .send({ name: '새로운 한재훈' })
+      .expect(200);
+  });
+  it('DELETE 200', () => {
+    return request(app.getHttpServer())
+      .delete('/users/1')
+      .expect(200);
+  });
+
+});
 });
