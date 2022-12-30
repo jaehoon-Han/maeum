da4a057fe958350669ff280137d1b5759db265db
jaehoon-Han <eroel1060@daum.net>
2022-12-30 22:07:14 +0900

    nest - DB

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,54 +8,54 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
 
-        constructor(private usersService: UsersService){};
+        constructor(private readonly usersService: UsersService) {};
 
 
-        // @Get()
-        // findAll(): Promise<User[]> {
-        //     return this.usersService.findAll();
-        // }
+        @Get()
+        getUsers(): Promise<User[]> {
+            return this.usersService.findAll();
+        }
 
-        // @Get(':id')
-        // findOne(@Param('id')id:number): Promise<User>{
-        //     return this.usersService.findOne(id);
-        // }
+        @Get(':id')
+        findOne(@Param('id')id:number): Promise<User>{
+            return this.usersService.findOne(id);
+        }
 
-        // @Post()
-        // create(@Body() user: User){
-        //     return this.usersService.create(user);
-        // }
+        @Post()
+        create(@Body() user: User){
+            return this.usersService.create(user);
+        }
 
-        // @Delete(':id')
-        // remote(@Param( 'id' )id: number){
-        //     this.usersService.remove(id);
-        // }
+        @Delete(':id')
+        remote(@Param( 'id' )id: number){
+            this.usersService.remove(id);
+        }
 
 
 
-    @Get()
-    getAllUser(): User[] {
-        return this.usersService.getAllUser();
-    }
+    // @Get()
+    // getAllUser(): User[] {
+    //     return this.usersService.getAllUser();
+    // }
 
-    @Get("/:id")
-    getOneUser(@Param('id') userId: number):User {
-        return this.usersService.getOneUser(userId);
-    }
+    // @Get("/:id")
+    // getOneUser(@Param('id') userId: number):User {
+    //     return this.usersService.getOneUser(userId);
+    // }
 
-    @Post()
-    createUser(@Body() userData: CreateUserDto) {
-       return this.usersService.createUser(userData);
-    }
+    // @Post()
+    // createUser(@Body() userData: CreateUserDto) {
+    //    return this.usersService.createUser(userData);
+    // }
 
-    @Delete('/:id')
-    removeUser(@Param('id') userId: number) {
-        return this.usersService.deleteOneUser(userId);
-    }
+    // @Delete('/:id')
+    // removeUser(@Param('id') userId: number) {
+    //     return this.usersService.deleteOneUser(userId);
+    // }
 
-    @Patch('/:id')
-    patchUser(@Param('id') userId: number, @Body() updateData: UpdateUserDto) {
-       return this.usersService.update(userId, updateData);
-    }
+    // @Patch('/:id')
+    // patchUser(@Param('id') userId: number, @Body() updateData: UpdateUserDto) {
+    //    return this.usersService.update(userId, updateData);
+    // }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,67 +4,66 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { userRepository } from './entities/user.repository';
 
 @Injectable()
 export class UsersService {
     // private readonly user: User[] = [];
     
-    // constructor(
-    //     @InjectRepository(User)
-    //     private usersRepository: Repository<User>,
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
 
-    // ){}
+    ) {}
 
-    private users: User[] = [];
+    findAll(): Promise<User[]> {
+        return this.usersRepository.find();
+    }
 
-    // findAll(): Promise<User[]> {
-    //     return this.usersRepository.find();
+    findOne(id: number): Promise<User> {
+        return this.usersRepository.findOne({where:{id}});
+    }
+
+    async create(user: User): Promise<void> {
+        await this.usersRepository.save(user);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.usersRepository.delete(id);
+    }
+
+    // getAllUser(): User[] {
+    //     return this.users;
+    //     // 진짜 데이터베이스면 return에 Query가 와야한다.
     // }
 
-    // findOne(id: number): Promise<User> {
-    //     return this.usersRepository.findOne({where:{id}});
+    // getOneUser(id: number): User {
+    //     const user = this.users.find(user => user.id === id);
+    //     if (!user) {
+    //         throw new NotFoundException(`User with ID ${id} not found.`);
+    //     }
+    //     return user;
     // }
 
-    // async create(user: User): Promise<void> {
-    //     await this.usersRepository.save(user);
+    // deleteOneUser(id: number) {
+    //     this.getOneUser(id);
+    //     this.users = this.users.filter(user => user.id !== id);
     // }
 
-    // async remove(id: number): Promise<void> {
-    //     await this.usersRepository.delete(id);
+    // createUser(userData: CreateUserDto) {
+    //     this.users.push({
+    //         id: this.users.length + 1,
+    //         ...userData,
+    //     });
+
+
     // }
 
-    getAllUser(): User[] {
-        return this.users;
-        // 진짜 데이터베이스면 return에 Query가 와야한다.
-    }
-
-    getOneUser(id: number): User {
-        const user = this.users.find(user => user.id === id);
-        if (!user) {
-            throw new NotFoundException(`User with ID ${id} not found.`);
-        }
-        return user;
-    }
-
-    deleteOneUser(id: number) {
-        this.getOneUser(id);
-        this.users = this.users.filter(user => user.id !== id);
-    }
-
-    createUser(userData: CreateUserDto) {
-        this.users.push({
-            id: this.users.length + 1,
-            ...userData,
-        });
-
-
-    }
-
-    update(id: number, updateData: UpdateUserDto) {
-        const user = this.getOneUser(id);
-        this.deleteOneUser(id);
-        this.users.push({ ...user, ...updateData });
-    }
+    // update(id: number, updateData: UpdateUserDto) {
+    //     const user = this.getOneUser(id);
+    //     this.deleteOneUser(id);
+    //     this.users.push({ ...user, ...updateData });
+    // }
 }
 
 
