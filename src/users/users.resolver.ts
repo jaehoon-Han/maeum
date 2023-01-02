564b52229e2cde8a09664fc98b652578393e8493
jaehoon-Han <eroel1060@daum.net>
2023-01-03 06:22:18 +0900

    final : 마음연구소 과제 제출

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,67 @@
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateUserInput, CreateUserOutput } from './dto/create-user.dto';
+import { DeleteUserOutput } from './dto/delete-user.dto';
+import { EditProfileInput, EditProfileOutput } from './dto/edit-profile.dto';
+import { LoginInput, LoginOutput } from './dto/login.dto';
+import { UserProfileInput, UserProfileOutput } from './dto/user-profile.dto';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Query(() => Boolean)
+  hi(): boolean {
+    return true;
+  }
+
+  // create User 계정 생성
+  @Mutation(() => CreateUserOutput)
+  async createUser(
+    @Args('input') createUserInput: CreateUserInput,
+  ): Promise<CreateUserOutput> {
+    return this.usersService.createUser(createUserInput);
+  }
+
+  // login 로그인
+  @Mutation(() => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    return await this.usersService.login(loginInput);
+  }
+
+  @Query(() => User)
+  @UseGuards(AuthGuard)
+  me(@AuthUser() authUser: User) {
+    return authUser;
+  }
+
+  // user Profile 유저 프로필
+  @Query(() => UserProfileOutput)
+  @UseGuards(AuthGuard)
+  userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    return this.usersService.findById(userProfileInput.userId);
+  }
+
+  // edit Profile 프로필 수정
+  @UseGuards(AuthGuard)
+  @Mutation(() => EditProfileOutput)
+  editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    return this.usersService.editProfile(authUser.id, editProfileInput);
+  }
+
+  // delete User 계정 삭제
+  @UseGuards(AuthGuard)
+  @Mutation(() => DeleteUserOutput)
+  deleteUser(@AuthUser() authUser: User): Promise<DeleteUserOutput> {
+    return this.usersService.deleteUser(authUser.id);
+  }
+}
